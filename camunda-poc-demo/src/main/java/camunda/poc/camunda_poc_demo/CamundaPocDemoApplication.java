fcb526a4946ecc96d73a555671555b6850fe64db
Bongumenzi Simelane <96014071+Menzi1965@users.noreply.github.com>
2025-03-19 13:24:59 +0200

    Add files via upload

---
+++ camunda-poc-demo/src/main/java/camunda/poc/camunda_poc_demo/CamundaPocDemoApplication.java
@@ -0,0 +1,13 @@
+package camunda.poc.camunda_poc_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaPocDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaPocDemoApplication.class, args);
+	}
+
+}
---
+++ camunda-poc-demo/src/test/java/camunda/poc/camunda_poc_demo/CamundaPocDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package camunda.poc.camunda_poc_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaPocDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
